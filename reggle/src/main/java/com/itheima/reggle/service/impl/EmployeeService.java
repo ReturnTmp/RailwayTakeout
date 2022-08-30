58472a010eba3bb8cbb69d2cbc28bc841b9c4ee0
ReturnTmp <3055871692@qq.com>
2022-08-30 09:56:31 +0800

    添加reggle模块

---
+++ reggle/src/main/java/com/itheima/reggle/service/impl/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.itheima.reggle.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggle.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
