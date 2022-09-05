559d76f3d2e335c1b5e224a02844e9450952a765
Mayf1ower <1321574993@qq.com>
2022-09-05 16:49:14 +0800

    完善员工禁用、删除操作

--- railway_take_out/src/main/java/com/itheima/railway/controller/EmployeeController.java
+++ railway_take_out/src/main/java/com/itheima/railway/controller/EmployeeController.java
@@ -8,24 +8,15 @@ import com.itheima.railway.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Set;
 
 @Slf4j
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-
-    @Autowired
-    private RedisTemplate redisTemplate;
 
     @Autowired
     private EmployeeService employeeService;
@@ -86,15 +77,12 @@ public class EmployeeController {
      * @return
      */
     @PostMapping
-    @CachePut(value = "employeeCache",key = "#employee.id")
-    @CacheEvict(value = "employeePageCache",allEntries = true)
     public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
         log.info("新增员工，员工信息：{}",employee.toString());
 
         //设置初始密码123456，需要进行md5加密处理
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
 
-        //下述字段都是使用MP的公共字段填充
         //employee.setCreateTime(LocalDateTime.now());
         //employee.setUpdateTime(LocalDateTime.now());
 
@@ -105,11 +93,6 @@ public class EmployeeController {
         //employee.setUpdateUser(empId);
 
         employeeService.save(employee);
-
-        //利用redisTemplate实现page的模糊删除
-//        String key="userPageCache*";
-//        Set keys = redisTemplate.keys(key);
-//        redisTemplate.delete(keys);
 
         return R.success("新增员工成功");
     }
@@ -122,7 +105,6 @@ public class EmployeeController {
      * @return
      */
     @GetMapping("/page")
-    @Cacheable(value = "employeePageCache",key = "#page+'-'+#pageSize+'-'+#name")
     public R<Page> page(int page,int pageSize,String name){
         log.info("page = {},pageSize = {},name = {}" ,page,pageSize,name);
 
@@ -148,15 +130,14 @@ public class EmployeeController {
      * @return
      */
     @PutMapping
-    @CacheEvict(value = "employeeCache",key = "#employee.id")
     public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
         log.info(employee.toString());
 
         long id = Thread.currentThread().getId();
         log.info("线程id为：{}",id);
-        //Long empId = (Long)request.getSession().getAttribute("employee");
-        //employee.setUpdateTime(LocalDateTime.now());
-        //employee.setUpdateUser(empId);
+        if(employee.getId()==1&&employee.getStatus()==0){
+            return R.error("无法禁用管理员");
+        }
         employeeService.updateById(employee);
 
         return R.success("员工信息修改成功");
@@ -168,15 +149,11 @@ public class EmployeeController {
      * @return
      */
     @DeleteMapping
-    @Caching(evict = {
-            @CacheEvict(value = "employeeCache",key = "#id"),
-            @CacheEvict(value = "employeePageCache",allEntries = true)
-    })
     public R<String> delete(Long id){
         log.info("删除员工,id为:{}",id);
 
         if(id==1){
-            return R.error("管理员不可删除，删除失败");
+            return R.error("无法删除管理员");
         }
 
         employeeService.removeById(id);
@@ -189,7 +166,6 @@ public class EmployeeController {
      * @return
      */
     @GetMapping("/{id}")
-    @Cacheable(value = "userCache",key = "#id",unless = "#result.code==0")
     public R<Employee> getById(@PathVariable Long id){
         log.info("根据id查询员工信息...");
         Employee employee = employeeService.getById(id);
